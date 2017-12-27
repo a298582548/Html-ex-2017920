2224199bb25261d9fb0c328a49d6119ee5f4b424
a298582548 <105021010@live.asia.edu.tw>
2017-12-27 15:43:04 +0800

    資料庫

---
+++ Connect.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: user
+ * Date: 2017/12/27
+ * Time: 下午 02:41
+ */
+$db_server = "localhost";
+//資料庫名稱
+$db_name = "105021010";
+//資料庫管理者帳號
+$db_user = "105021010";
+//資料庫管理者密碼
+$db_passwd = "#mEM1RRLe";
+
+//對資料庫連線
+$connect = @mysqli_connect($db_server, $db_user, $db_passwd,$db_name);
+
+mysqli_set_charset($connect,'utf8');
+if(!$connect)
+    die("無法對資料庫連線");
+//資料庫連線採UTF8
+
+
+mysqli_set_charset($connect,'utf8');
+////選擇資料庫
+//if(!@mysqli_select_db($db_name))
+//    echo"無法使用資料庫";
+//?>
---
+++ login.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: user
+ * Date: 2017/12/27
+ * Time: 下午 01:15
+ */
+
+    ?>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<form name="form" method="post" action="Connect.php">
+    <input type="text" name="id" /> <br>
+    <input type="password" name="pw" /> <br>
+    <input type="submit" name="button" value="登入" />
+    <a href="register.php">申請帳號</a>
+</form>
